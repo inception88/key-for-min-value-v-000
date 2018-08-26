21981a1a65d6a9a2be75df8ae322064c2ec6dd35
Benjamin Jones <bljones36@gmail.com>
2018-08-26 16:24:52 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,14 +4,14 @@
 def key_for_min_value(hash)
   i = 0
   lowest1 = :key
-  hash.each do |key, value|
+  hash.each do |name, value|
     if i == 0
       lowest1 = key
       lowest = value
       i =+ 1
     end
    if value < lowest
-    lowest1 = key
+    lowest1 = name
     lowest = value
     end
   end
